fd9d7c0ca052d5bb9c903cfe7b774b06b9fd7fef
furlukine <erwan.ansquer@gmail.com>
2024-07-11 22:13:04 +0200

    Enabling vial

--- keyboards/furlukin/furlucorne/keymaps/vial/config.h
+++ keyboards/furlukin/furlucorne/keymaps/vial/config.h
@@ -1,7 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
 #pragma once
 
-#define SERIAL_USART_FULL_DUPLEX
-#define SERIAL_USART_TX_PIN GP0
-#define SERIAL_USART_RX_PIN GP1
-
-#define EE_HANDS
+#define VIAL_KEYBOARD_UID {0x24, 0x0C, 0x01, 0x6A, 0x8A, 0x68, 0x36, 0xA8}
+#define VIAL_UNLOCK_COMBO_ROWS {0,0}
+#define VIAL_UNLOCK_COMBO_COLS {0,1}
