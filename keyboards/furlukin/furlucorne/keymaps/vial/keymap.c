f27db429101e8e9e0c365f49d101ee71db760143
furlukine <erwan.ansquer@gmail.com>
2024-07-12 07:29:19 +0200

    Adding modifications

--- keyboards/furlukin/furlucorne/keymaps/vial/keymap.c
+++ keyboards/furlukin/furlucorne/keymaps/vial/keymap.c
@@ -27,14 +27,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	),
     [1] = LAYOUT_split_3x6_3(
         KC_F1,      KC_F2,  KC_F3,  KC_F4,    KC_F5,    KC_F6,                    KC_PSLS,      KC_P7,      KC_P8,	    KC_KP_9,   	KC_NO,		  KC_PSCR,
-        KC_F7,		KC_F8,  KC_F9,	KC_F10,   KC_F11,   KC_F12,                   KC_PAST,      KC_P4,      KC_P5,	    KC_P6,   	KC_PENT,	  _______,
-        _______, 	KC_NO,	KC_NO, 	KC_NO,    KC_NO,    KC_NO,                    KC_PMNS,  	KC_P1,	    KC_P2,	    KC_P3,	    KC_PDOT,	  _______,
+        KC_F7,		KC_F8,  KC_F9,	KC_F10,   KC_F11,   KC_F12,                   KC_PAST,      KC_P4,      KC_P5,	    KC_P6,   	KC_PENT,	  KC_TRNS,
+        KC_TRNS, 	KC_NO,	KC_NO, 	KC_NO,    KC_NO,    KC_NO,                    KC_PMNS,  	KC_P1,	    KC_P2,	    KC_P3,	    KC_PDOT,	  KC_TRNS,
 									KC_LALT,  KC_ESC,   KC_NO, 	                  KC_KP_PLUS,	KC_KP_0,    KC_NO
 	),
     [2] = LAYOUT_split_3x6_3(
-        _______,	FR_LPRN,   FR_RPRN,     FR_LCBR,  FR_RCBR,  FR_PIPE,              KC_NO,      KC_NO,	  KC_NO,		KC_SCRL,    KC_PAUS,    KC_PSCR,
-        _______,	FR_AT,     FR_CCED,	    FR_CIRC,  FR_DIAE,  FR_UGRV,              KC_PGUP,    KC_HOME,	  KC_UP,		KC_END,    	KC_NO,		KC_NO,
-        _______, 	KC_NO,     FR_BSLS,     FR_SLSH,  FR_LBRC,  FR_RBRC,              KC_PGDN,    KC_LEFT,    KC_DOWN,		KC_RGHT,    KC_NO,	    KC_NO,
+        KC_TRNS,	FR_LPRN,   FR_RPRN,     FR_LCBR,  FR_RCBR,  FR_PIPE,              KC_NO,      KC_NO,	  KC_NO,		KC_SCRL,    KC_PAUS,    KC_PSCR,
+        KC_TRNS,	FR_AT,     FR_CCED,	    FR_CIRC,  FR_DIAE,  FR_UGRV,              KC_PGUP,    KC_HOME,	  KC_UP,		KC_END,    	KC_NO,		KC_NO,
+        KC_TRNS, 	KC_NO,     FR_BSLS,     FR_SLSH,  FR_LBRC,  FR_RBRC,              KC_PGDN,    KC_LEFT,    KC_DOWN,		KC_RGHT,    KC_NO,	    KC_NO,
 									    	KC_LGUI,  KC_SPC,   KC_NO, 	              KC_NO,      KC_NO,  	  KC_NO
 	),
     [3] = LAYOUT_split_3x6_3(         
